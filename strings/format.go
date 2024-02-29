14817f16801d3a771db8ec3b60217cbaf43d20a1
Zero <tobewhatwewant@gmail.com>
2024-02-29 14:53:31 +0800

    refact(strings): refactor strings.Format use {{VAR}}

--- strings/format.go
+++ strings/format.go
@@ -2,26 +2,27 @@ package strings
 
 import (
 	"regexp"
+	"strings"
 
 	"github.com/go-zoox/core-utils/cast"
 	"github.com/go-zoox/core-utils/object"
 )
 
 // FormatRe is the regular expression for format.
-const FormatRe = `\{[^\}]+\}`
+const FormatRe = `\{\{([^\}]+)\}\}`
 
 // Format formats a string with template bracket {key}}} and value map.
 func Format(pattern string, data map[string]any) string {
-	bytes := regexp.MustCompile(FormatRe).ReplaceAllFunc([]byte(pattern), func(b []byte) []byte {
-		key := string(b[1 : len(b)-1])
+	re := regexp.MustCompile(FormatRe)
+
+	return re.ReplaceAllStringFunc(pattern, func(m string) string {
+		key := strings.Trim(m, "{}")
 		value := object.Get(data, key)
 
 		if value == nil {
-			return nil
+			return ""
 		}
 
-		return []byte(cast.ToString(value))
+		return cast.ToString(value)
 	})
-
-	return string(bytes)
 }
